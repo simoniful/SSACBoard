eddeb60e95190bd788108aec763092b6333bf729
sanghunlee <simoniful0@gmail.com>
2021-12-30 19:50:00 +0900

    Initial Commit

---
+++ SSAC_Board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Board
+//
+//  Created by Sang hun Lee on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
